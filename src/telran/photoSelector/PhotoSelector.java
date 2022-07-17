895ef976f3def666ae1fb9ebd4a1d7b2b6b373c8
aleksandrtomash <tombasket508@gmail.com>
2022-07-17 19:19:57 +0300

    HW-PhotoSelector

--- src/telran/photoSelector/PhotoSelector.java
+++ src/telran/photoSelector/PhotoSelector.java
@@ -5,7 +5,22 @@ import java.util.regex.Pattern;
 
 public class PhotoSelector {
 
-			
+	private static final String pictures[] = {
+			"Paris\\20140101_090000.jpg",
+			"Paris\\20140201_121005.jpg",				
+			"Paris\\20150301_211035.jpg",				
+			"Paris\\20150401_110023.gif",
+			"Paris\\20150401_181705.jpg",				
+			"Paris\\20150501_231035.gif",				
+			"London\\20140205_090000.jpg",
+			"London\\20140205_121005.jpg",				
+			"London\\20140601_211035.gif",				
+			"London\\20151001_110023.jpg",
+			"London\\20151001_121705.jpg",				
+			"London\\20151001_231035.jpg",
+			"Chicago\\20150301_120001.png",
+			"Chicago\\20151111_232000.png"
+		};
 
 			static void selectPictures(String title, String regex) {
 				System.out.printf("%n%s%n------------------------%n", title);
